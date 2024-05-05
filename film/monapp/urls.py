653116b2c2cf849c137e5f7abbcfd1bbf66f91c6
Arnaud Rein <arnaud.rein029@gmail.com>
2024-05-05 16:35:34 +0200

    Affichage d'une donnée à l'url /film

--- film/monapp/urls.py
+++ film/monapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
+from .views import filmListView
 
 
 
 
-# urlpatterns = [
-#     path("/", , name="school"),
+urlpatterns = [
+    path("/", filmListView.as_view(), name="movie"),
    
   
-# ]
+]
--- film/monapp/views.py
+++ film/monapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from .models import Film 
+from django.views.generic import ListView
 
 # Create your views here.
+
+
+class filmListView(ListView):
+    model = Film
+    template_name = 'all_movies.html'
+    context_object_name = 'films'
+    
+    queryset = Film.objects.all()
