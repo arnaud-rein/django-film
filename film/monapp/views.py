3b18304f16b71bd1fabe1c460ba506c0edd75390
Arnaud Rein <arnaud.rein029@gmail.com>
2024-05-05 17:35:39 +0200

    search bar fonctionnelle pour rechercher dans la base de données

--- film/monapp/views.py
+++ film/monapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Film 
 from django.views.generic import ListView
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -11,3 +12,13 @@ class filmListView(ListView):
     context_object_name = 'films'
     
     queryset = Film.objects.all()
+
+def search(request):
+    if request.method == 'POST':
+        search = request.POST['searched']
+        films = Film.objects.filter(title__contains=search)
+        return render(request, 'search_bar.html', {'films': films
+                                                #    , 'search': search
+                                                   })
+    else:    
+        return render(request, 'search_bar.html')
