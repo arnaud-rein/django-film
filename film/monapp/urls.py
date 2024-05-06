9fd53c466d4dcc63ca313d5002fbf4200333bd67
Arnaud Rein <arnaud.rein029@gmail.com>
2024-05-06 11:16:11 +0200

    affichage de l'api quand on fait une recherche dans la search bar

--- film/monapp/urls.py
+++ film/monapp/urls.py
@@ -8,7 +8,9 @@ from .views import filmListView
 urlpatterns = [
     path("/", filmListView.as_view(), name="movie"),
     path("/search", views.search, name="search"),
+    path("/api", views.movie_view, name="api"),
     path('film/<int:film_id>/', views.film_detail, name='film_detail'),
+    path('film/<str:title>/', views.movie_view, name='film_name'),
    
   
 ]
--- film/monapp/views.py
+++ film/monapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+import requests
 from .models import Film 
 from django.views.generic import ListView
 from django.http import HttpResponseRedirect
+from urllib.parse import quote
 
 # Create your views here.
 
@@ -13,17 +15,70 @@ class filmListView(ListView):
     
     queryset = Film.objects.all()
 
+# def search(request):
+#     if request.method == 'POST':
+#         search = request.POST['searched']
+#         films = Film.objects.filter(title__contains=search)
+#         return render(request, 'search_bar.html', {'films': films})    
+
+#     else:    
+#         return render(request, 'search_bar.html')   
+
+
 def search(request):
     if request.method == 'POST':
-        search = request.POST['searched']
-        films = Film.objects.filter(title__contains=search)
-        return render(request, 'search_bar.html', {'films': films
-                                                #    , 'search': search
-                                                   })
-    else:    
-        return render(request, 'search_bar.html')
+        print('dans le post')
+        searched = request.POST['searched']
+        films = Film.objects.filter(title__icontains=searched)
+        print("films => ")
+        print(films)
+        if not films.exists():
+            data = get_movie_data(searched)
+            print("data=>")
+            print(data)
+            if data is not None:
+                return render(request, 'movie_api.html', {'movies': data})
+            else:
+                return render(request, 'error_template.html', {'error': 'Failed to retrieve data'})
+        return render(request, 'search_bar.html', {'films': films, 'searched': searched})
+    else:
+        return render(request, 'search_bar.html', {})    
     
 
 def film_detail(request, film_id):
     film = Film.objects.get(id=film_id)
     return render(request, 'film_detail.html', {'film': film})
+
+
+
+def get_movie_data(title):
+    try:
+        title = quote(title)
+        url = f"https://www.omdbapi.com/?t={title}&apikey=5e975dd0"
+        response = requests.get(url)
+        print("api rsponse => " +  response.text) 
+        response.raise_for_status()  # Lève une exception pour les codes 4XX ou 5XX
+        return response.json()
+    except requests.RequestException as e:
+        print(e)
+        return None
+
+
+# def movie_view(request, title):
+#     data = get_movie_data()
+#     if data is not None:
+#         return render(request, 'movie_api.html', {'movies': data})
+#     else:
+#         return render(request, 'error_template.html', {'error': 'Failed to retrieve data'})
+    
+
+def movie_view(request, title):
+    try:
+        film = Film.objects.get(title=title)
+        return render(request, 'film_detail.html', {'film': film})
+    except Film.DoesNotExist:
+        data = get_movie_data(title)
+        if data is not None:
+            return render(request, 'movie_api.html', {'movies': data})
+        else:
+            return render(request, 'error_template.html', {'error': 'Failed to retrieve data'})
